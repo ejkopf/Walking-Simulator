a76ba8c6948d394e497d604b219c7733cab93c4d
Eliot Kopf <eliotkopf@Eliots-MacBook-Air.local>
2024-02-27 11:15:21 -0800

    walk button fixed

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -19,6 +19,9 @@ public class Walk : MonoBehaviour
     public Text steps;
 
     private float stepcount;
+    public float seconds;
+
+    private bool walking;
 
     private CapstoneLogger logger;
     // Start is called before the first frame update
@@ -26,27 +29,40 @@ public class Walk : MonoBehaviour
     {
         CapstoneLogger logger = new CapstoneLogger(20240109, "walkingsim", "860d0f1dd48e31e2fb5898f5e1cb101d", 1);
         this.logger = logger;
+
+        walking = false;
+        seconds = 0f;
+        stepcount = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         steps.text = stepcount.ToString();
+        seconds += 0.004f;
+        if (walking)
+        {
+            if (seconds > 1f)
+            {
+                seconds = 0f;
+                stepcount++;
+            }
+            UpdatePosition(phone);
+            UpdatePosition(maincamera);
+            UpdatePosition(walkButton);
+            UpdatePosition(clouds);
+            UpdatePosition(phoneButton);
+            UpdatePosition(back);
+            tutorialtext.transform.position = new Vector3(tutorialtext.transform.position.x, tutorialtext.transform.position.y, tutorialtext.transform.position.z + 0.5f);
+            notificationtext.transform.position = new Vector3(notificationtext.transform.position.x, notificationtext.transform.position.y, notificationtext.transform.position.z + 0.5f);
+        }
     }
 
     void OnMouseDown()
     {
         logger.LogActionWithNoLevel(0, "walk");
-        stepcount++;
-        UpdatePosition(phone);
-        UpdatePosition(maincamera);
-        UpdatePosition(walkButton);
-        UpdatePosition(clouds);
-        UpdatePosition(phoneButton);
-        UpdatePosition(back);
-        tutorialtext.transform.position = new Vector3(tutorialtext.transform.position.x, tutorialtext.transform.position.y, tutorialtext.transform.position.z + 0.5f);
-        notificationtext.transform.position = new Vector3(notificationtext.transform.position.x, notificationtext.transform.position.y, notificationtext.transform.position.z + 0.5f);
+        walking = !walking;
     }
 
-    void UpdatePosition(GameObject obj) => obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z + 0.5f);
+    void UpdatePosition(GameObject obj) => obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z + 0.01f);
 }
