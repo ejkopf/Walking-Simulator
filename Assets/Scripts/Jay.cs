f47e74fe55404479604d2a521e4e2ca92e9eabed
vee-effekt <gannet@cs.washington.edu>
2024-03-04 00:23:43 -0800

    dialogue

--- Assets/Scripts/Jay.cs
+++ Assets/Scripts/Jay.cs
@@ -31,20 +31,33 @@ public class Jay : MonoBehaviour {
     // second
     public GameObject something;
     public GameObject actually;
+    public GameObject nbd;
+    // third
+    public GameObject roommate;
+    public GameObject isThatIt;
+    public GameObject worried;
+    public GameObject dont;
+    public GameObject no;
+    // fourth
+    public GameObject noRepeat;
+    public GameObject quick;
+    public GameObject bad;
+    public GameObject question;
+    public GameObject moreQuestion;
 
     // options
     public GameObject responseContainer;
     public GameObject responseContainerTwo;
-    // first
-    public GameObject optFine;
-    public GameObject optNotFine;
-    // second
-    public GameObject optSomething;
-    public GameObject optActually;
+    public GameObject responseContainerThree;
+    public GameObject responseContainerFour;
+    public GameObject responseContainerFive;
+    public GameObject responseContainerSix;
 
+    // screens
     public GameObject startScreen;
     public GameObject screen1;
     public GameObject screen2;
+    public GameObject screen3;
 
     // progression
     private bool firstJay;
@@ -55,6 +68,15 @@ public class Jay : MonoBehaviour {
     private bool secondPlayer;
     private bool thirdJay;
     private bool entryPoint;
+    private bool thirdPlayer;
+    private bool fourthJay;
+    private bool fourthPlayer;
+    private bool fifthJay;
+    private bool fifthPlayer;
+    private bool sixthJay;
+    private bool sixthPlayer;
+    private bool seventhJay;
+    private bool seventhPlayer;
     // endstate
     private bool deadEnd;
 
@@ -68,9 +90,12 @@ public class Jay : MonoBehaviour {
         startScreen.SetActive(false);
         screen1.SetActive(false);
         screen2.SetActive(false);
+        screen3.SetActive(false);
 
         deactivateScreenOne();
         deactivateScreenTwo();
+        deactivateScreenThree();
+        deactivateScreenFour();
 
         firstJay = false;
         firstPlayer = false;
@@ -79,24 +104,42 @@ public class Jay : MonoBehaviour {
         saidFine = false;
         saidNotReally = false;
         thirdJay = false;
+        thirdPlayer = false;
+        fourthJay = false;
+        fourthPlayer = false;
+        fifthJay = false;
+        fifthPlayer = false;
+        sixthJay = false;
+        sixthPlayer = false;
+        seventhJay = false;
+        seventhPlayer = false;
     }
     void Update () {
         if (startScreen.activeInHierarchy) {
             screen1.SetActive(true);
             screen2.SetActive(true);
+            screen3.SetActive(true);
             serveCurrentScreen();
         } else {
             deactivateScreenOne();
             deactivateScreenTwo();
+            deactivateScreenThree();
+            deactivateScreenFour();
         }
     }
 
     void serveCurrentScreen() {
         if (!secondPlayer) {
             handleScreenOneState();
-        } else if (secondPlayer /* && !nthPlayer */) {
+        } else if (secondPlayer && !fourthJay) {
             deactivateScreenOne();
             handleScreenTwoState();
+        } else if (fourthJay && !fifthPlayer) {
+            deactivateScreenTwo();
+            handleScreenThreeState();
+        } else if (fifthPlayer) {
+            deactivateScreenThree();
+            handleScreenFourState();
         } else {
             // ... 
         }
@@ -120,7 +163,6 @@ public class Jay : MonoBehaviour {
             if (notreally.activeInHierarchy || fine.activeInHierarchy) {
                 if (!firstPlayer) {
                     firstPlayer = true;
-
                     if (notreally.activeInHierarchy) {
                         saidNotReally = true;
                     } else {
@@ -195,10 +237,6 @@ public class Jay : MonoBehaviour {
         whatsUp.SetActive(false);
         responseContainer.SetActive(false);
         responseContainerTwo.SetActive(false);
-        optFine.SetActive(false);
-        optNotFine.SetActive(false);
-        optActually.SetActive(false);
-        optSomething.SetActive(false);
     }
 
     void deactivateScreenTwo() {
@@ -206,6 +244,27 @@ public class Jay : MonoBehaviour {
         actually.SetActive(false);
         specifically.SetActive(false);
         saySo.SetActive(false);
+        nbd.SetActive(false);
+        responseContainerThree.SetActive(false);
+    }
+
+    void deactivateScreenThree() {
+        roommate.SetActive(false);
+        isThatIt.SetActive(false);
+        worried.SetActive(false);
+        dont.SetActive(false);
+        no.SetActive(false);
+        responseContainerFour.SetActive(false);
+        responseContainerFive.SetActive(false);
+    }
+
+    void deactivateScreenFour() {
+        noRepeat.SetActive(false);
+        quick.SetActive(false);
+        bad.SetActive(false);
+        question.SetActive(false);
+        moreQuestion.SetActive(false);
+        responseContainerSix.SetActive(false);
     }
 
     void handleScreenTwoState() {
@@ -221,7 +280,7 @@ public class Jay : MonoBehaviour {
                 saySo.SetActive(true);
                 thirdJay = true;
             }
-        } else if (secondPlayer && thirdJay) {
+        } else if (thirdJay && !thirdPlayer) {
             if (!deadEnd) {
                 something.SetActive(true);
                 specifically.SetActive(true);
@@ -229,9 +288,97 @@ public class Jay : MonoBehaviour {
                 actually.SetActive(true);
                 saySo.SetActive(true);
             }
-
+            responseContainerThree.SetActive(true);
+            if (nbd.activeInHierarchy) {
+                thirdPlayer = true;
+                entryPoint = false;
+            }
+        } else if (thirdPlayer && !fourthJay) {
+            if (!entryPoint) {
+                playerResponseTime = Time.time;
+                entryPoint = true;
+            }
+            something.SetActive(true);
+            specifically.SetActive(true);
+            if (nbd.activeInHierarchy && (Time.time - playerResponseTime) > (time * 2f)) {
+                fourthJay = true;
+                entryPoint = false;
+            }
         } else {
             deactivateScreenTwo();
+        }
+    }
+
+    void handleScreenThreeState() {
+        if (fourthJay && !fourthPlayer) {
+            roommate.SetActive(true);
+            isThatIt.SetActive(true);
+            responseContainerFour.SetActive(true);
+            if (worried.activeInHierarchy) {
+                fourthPlayer = true;
+            }
+        } else if (fourthPlayer && !fifthJay) {
+            if (!entryPoint) {
+                playerResponseTime = Time.time;
+                entryPoint = true;
+            }
+            isThatIt.SetActive(true);
+            worried.SetActive(true);
+            if ((Time.time - playerResponseTime) > (time * 2f)) {
+                dont.SetActive(true);
+                fifthJay = true;
+            }
+        } else if (fifthJay && !fifthPlayer) {
+            isThatIt.SetActive(true);
+            worried.SetActive(true);
+            dont.SetActive(true);
+            responseContainerFive.SetActive(true);
+            if (no.activeInHierarchy) {
+                no.SetActive(true);
+                fifthPlayer = true;
+                entryPoint = false;
+            }
+        } else {
+            deactivateScreenThree();
+        }
+    }
+
+    void handleScreenFourState() {
+        if (fifthPlayer && !sixthJay) {
+            if (!entryPoint) {
+                playerResponseTime = Time.time;
+                entryPoint = true;
+            }
+            noRepeat.SetActive(true);
+            if ((Time.time - playerResponseTime) > 5f) {
+                quick.SetActive(true);
+                sixthJay = true;
+            }
+        } else if (sixthJay && !sixthPlayer) {
+            noRepeat.SetActive(true);
+            quick.SetActive(true);
+            responseContainerSix.SetActive(true);
+            if (bad.activeInHierarchy) {
+                bad.SetActive(true);
+                sixthPlayer = true;
+                entryPoint = false;
+            }
+        } else if (sixthPlayer && !seventhJay) {
+            if (!entryPoint) {
+                playerResponseTime = Time.time;
+                entryPoint = true;
+            }
+            noRepeat.SetActive(true);
+            quick.SetActive(true);
+            bad.SetActive(true);
+            if ((Time.time - playerResponseTime) > 2f) {
+                question.SetActive(true);
+            }
+            if ((Time.time - playerResponseTime) > 65f) {
+                moreQuestion.SetActive(true);
+            }
+        } else {
+            deactivateScreenFour();
         }
     }
 }
